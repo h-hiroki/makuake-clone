7e104ffa096e25d7fe874a8410f03d9f3c029a77
h-hiroki <gyouzakuuka@gmail.com>
2018-01-02 00:28:55 +0900

    commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ApplicationRecord
   has_many :courses
   accepts_nested_attributes_for :courses
 
-  validates :title,       presence: true,      length: { maximum: 50 }
+  validates :title,       presence: true,      length: { maximum: 20 }
   validates :target_fig,  presence: true,      length: { maximum: 7 }
   validates :end_time,    presence: true
   validates :image,       presence: true
